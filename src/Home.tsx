3e5ae0757a32857cfeff172fa02dbaebcf9e0136
Andres Castro <andrescastro2011@hotmail.com>
2022-06-07 19:44:11 -0300

    null en el length

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
  
         event.preventDefault();
         
-        if(value != null && value.length < 1){
+        if(value?.length < 1){
             return ;
         }
 
