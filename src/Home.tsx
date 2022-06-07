4f1ab047a089e183d80448cbf121f6dfb5fe6dc6
Andres Castro <andrescastro2011@hotmail.com>
2022-06-07 19:38:37 -0300

    error vercel

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,10 +17,10 @@ const Home: React.FC = () => {
  
         event.preventDefault();
         
-        if(value !=null && value.length < 1){
+        if(value != null && value.length < 1){
             return ;
         }
-        
+
         const newProductos =   {
             id : +new Date,
             value,
