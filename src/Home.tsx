3d1a4e986bca522d423e0401311f2a5cd556676a
Andres Castro <andrescastro2011@hotmail.com>
2022-06-07 19:30:52 -0300

    error en el home

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,9 +17,7 @@ const Home: React.FC = () => {
  
         event.preventDefault();
         
-        if(value.length < 1){
-            return ;
-        }
+       
         const newProductos =   {
             id : +new Date,
             value,
